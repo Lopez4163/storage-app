bdbcbafc28f66cb7c1cf81178130d04c860733d3
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2023-07-13 16:07:43 -0400

    Updated code with new changes

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { https: true },
 
   optimizeDeps: {
     exclude: ["url", "buffer", "events", "stream"],
-  },
-  Server: {
-    host: true,
-    port: 5173,
   },
 })
