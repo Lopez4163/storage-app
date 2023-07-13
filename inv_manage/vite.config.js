96fff5843c8bfd51a2f90456e0fe67e9e1fd4e26
Nicholas Lopez <64043616+Lopez4163@users.noreply.github.com>
2023-07-13 16:24:51 -0400

    Updated code with new changes

--- inv_manage/vite.config.js
+++ inv_manage/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import mkcert from "vite-plugin-mkcert"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { https: true },
+
+  server: {
+    https: {
+      key: "ssl/key.pem",
+      cert: "ssl/cert.pem",
+    },
+  },
 
   optimizeDeps: {
     exclude: ["url", "buffer", "events", "stream"],
